7a43c666dd22a31a108f22ee6bf4fac688badefc
huangshiquan <1357626165@qq.com>
2020-06-21 00:21:40 +0800

    添加dll project

---
+++ ExcelHelper/ExcelHelper.cpp
@@ -0,0 +1,12 @@
+// ExcelHelper.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "ExcelHelper.h"
+
+// 这是已导出类的构造函数。
+// 有关类定义的信息，请参阅 ExcelHelper.h
+CExcelHelper::CExcelHelper()
+{
+    return;
+}
